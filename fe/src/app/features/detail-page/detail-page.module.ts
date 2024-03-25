25ea41c1134fd30bc2e7990e22ebd1f8fb885264
Do Hoang Duc <dohoangduc@luvina.net>
2024-03-25 16:09:06 +0700

    nopbai

--- fe/src/app/features/detail-page/detail-page.module.ts
+++ fe/src/app/features/detail-page/detail-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DetailPageRoutingModule } from './detail-page-routing.module';
 import { LayoutComponent } from './layout/layout.component';
+import { MainSharedModule } from 'src/app/shareds/main-shared/main-shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     CommonModule,
-    DetailPageRoutingModule
+    DetailPageRoutingModule,
+    MainSharedModule
   ]
 })
 export class DetailPageModule { }
